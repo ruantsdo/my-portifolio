03d9c64ec575bdb91f0c676c1170ecbc295aead0
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-09-29 19:09:10 -0300

    :heavy_plus_sign: build: Adding dependencies to the project
    Additionally the adjusted color palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,31 +1,32 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			bg: '#111827',
-  			bgContrast: '#1F2937',
-  			gray: '#444655',
-  			white: '#D9D9D9',
-  			blue: '#4191F9',
-  			blueSecondary: '#2E95B4',
-  			red: '#EE4266',
-  			purple: '#8957E5',
-  			green: '#04E824'
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      colors: {
+        bg: "#111827",
+        bgContrast: "#1F2937",
+        gray: "#444655",
+        white: "#D9D9D9",
+        blue: "#4191F9",
+        blueSecondary: "#2E95B4",
+        red: "#EE4266",
+        purple: "#8957E5",
+        green: "#04E824",
+        black: "#000000",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+    },
   },
   plugins: [require("tailwindcss-animate")],
 };
