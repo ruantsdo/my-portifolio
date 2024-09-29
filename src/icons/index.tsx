a30299abc93d1438da6bc9439e473e4b873f730a
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-09-29 19:11:50 -0300

    :sparkles: icons added

---
+++ src/icons/index.tsx
@@ -0,0 +1,22 @@
+//Illustrative Icons
+import Adaptive from "./illustrative/adaptive.svg";
+import Devices from "./illustrative/devices.svg";
+import Bulb from "./illustrative/light-bulb.svg";
+import PersonSignal from "./illustrative/person-signal.svg";
+import Rocket from "./illustrative/rocket.svg";
+
+//Social Icons
+import GitHub from "./social/github.svg";
+import LinkedIn from "./social/linkedIn.svg";
+import WhatsApp from "./social/whatsapp.svg";
+
+export {
+  Adaptive,
+  Devices,
+  Bulb,
+  PersonSignal,
+  Rocket,
+  GitHub,
+  LinkedIn,
+  WhatsApp,
+};
