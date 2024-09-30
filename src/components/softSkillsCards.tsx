37b313e30a1dc3f5ede67eeb364c89f3b735c985
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-09-30 08:39:32 -0300

    :iphone: removing global padding on base card header

--- src/components/softSkillsCards.tsx
+++ src/components/softSkillsCards.tsx
@@ -12,7 +12,7 @@ const SoftSkillsCards = () => {
         className="flex flex-row md:flex-col w-[90%] h-[140px] md:w-[250px] md:h-[220px] items-center"
         key={index}
       >
-        <CardHeader>
+        <CardHeader className="p-5">
           <div className="flex items-center bg-white rounded-full w-[60px] h-[60px]">
             <Image src={sk.icon} alt={sk.title} className="max-h-[75%]" />
           </div>
