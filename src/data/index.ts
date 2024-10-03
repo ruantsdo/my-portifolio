22d221ff37a19b9793e17c490280c5c1b700dfa4
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-02 21:50:17 -0300

    :card_file_box: data model for project added

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import SocialMediasData from "./socialMediaData";
 import SoftSkills from "./softSkillsData";
 import HardSkills from "./hardSkillsData";
+import Projects from "./projectsData";
 
-export { SocialMediasData, SoftSkills, HardSkills };
+export { SocialMediasData, SoftSkills, HardSkills, Projects };
