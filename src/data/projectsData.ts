6add121c4420f680c15cbfb5c9171856aa332730
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-04 11:01:04 -0300

    :card_file_box: Projects data updated

--- src/data/projectsData.ts
+++ src/data/projectsData.ts
@@ -1,17 +1,5 @@
 import { Project } from "@/types";
 
-const Projects: Project[] = [
-  {
-    title: "Projeto de teste",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptas, amet et aspernatur suscipit voluptates delectus iste, nisi minus sunt illum necessitatibus architecto! Deserunt sed explicabo consequatur dolor eius nisi.",
-    images: [
-      {
-        alt: "alt da imagem",
-        src: "https://miro.medium.com/v2/resize:fit:720/format:webp/1*t_G1kZwKv0p2arQCgYG7IQ.gif",
-      },
-    ],
-  },
-];
+const Projects: Project[] = [];
 
 export default Projects;
