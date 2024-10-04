72255d4c80e9ce3275ea11f810e87fd890138f78
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-04 11:06:12 -0300

    :sparkles: "ProjectDrawer " has been implemented

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,12 @@ import SocialMediaButtons from "./socialMediaButtons";
 import SoftSkillsCards from "./softSkillsCards";
 import HardSkillsCards from "./hardSkillsCards";
 import ProjectCard from "./ProjectCard/projectCard";
+import ProjectDrawer from "./ProjectCard/projectDrawer";
 
-export { SocialMediaButtons, SoftSkillsCards, HardSkillsCards, ProjectCard };
+export {
+  SocialMediaButtons,
+  SoftSkillsCards,
+  HardSkillsCards,
+  ProjectCard,
+  ProjectDrawer,
+};
