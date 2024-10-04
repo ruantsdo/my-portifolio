1002d7e1885378e57c571f79323dae7fee9dfa58
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-04 11:03:52 -0300

    :recycle: tooltip updated
    to avoid "hydratation" errors, tooltip was refactored

--- src/components/ProjectCard/tooltip.tsx
+++ src/components/ProjectCard/tooltip.tsx
@@ -1,14 +1,7 @@
-//Components
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 //Next
 import Image from "next/image";
 //Icons
-import { Media } from "@/icons";
+import { Media, GitHubAlternative, Eye } from "@/icons";
 //Types
 import { Project } from "@/types";
 
@@ -18,37 +11,25 @@ interface Props {
 
 const ProjectToolTip = ({ Project }: Props) => {
   return (
-    <TooltipProvider>
-      <Tooltip>
-        <TooltipTrigger className="hover:cursor-default">
-          {Project.videos ? (
-            <span className="absolute top-1 right-1 h-5 w-5">
-              <Image
-                src={Media}
-                alt="Media Icon"
-                className="absolute w-full h-full"
-              />
-              <Image
-                src={Media}
-                alt="Media Icon"
-                className="absolute w-full h-full animate-ping"
-              />
-            </span>
-          ) : (
-            <span className="absolute items-center top-2 right-3 h-3 w-3">
-              <span className="absolute w-full h-full rounded-full bg-sky-400 opacity-75 animate-ping " />
-              <span className="absolute w-full h-full rounded-full bg-sky-500" />
-            </span>
-          )}
-        </TooltipTrigger>
-        <TooltipContent className="text-center">
-          {Project.videos && <p>Video demonstrativo disponível</p>}
-          {Project.vercel && <p>Deploy disponível</p>}
-          {Project.github && <p>Código fonte disponível</p>}
-          <p>Clique no card para ver mais!</p>
-        </TooltipContent>
-      </Tooltip>
-    </TooltipProvider>
+    <div className="flex flex-row absolute top-1 justify-end gap-3 h-5 w-[95%]">
+      {Project.videos && (
+        <Image src={Media} alt="Media Icon" className="w-5 h-5 animate-tada" />
+      )}
+      {Project.demo && (
+        <Image
+          src={Eye}
+          alt="Demonstration Icon"
+          className="w-5 h-5 animate-tada"
+        />
+      )}
+      {Project.github && (
+        <Image
+          src={GitHubAlternative}
+          alt="GitHub Icon"
+          className="w-5 h-5 animate-tada"
+        />
+      )}
+    </div>
   );
 };
 
