e7b2e5cdb2f1c5344f1b5af41a2c8d1d4ac6518e
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-07 13:44:38 -0300

    :recycle: refactor: animation improved

--- src/sections/contact.tsx
+++ src/sections/contact.tsx
@@ -1,28 +1,23 @@
 // Components
-import { SocialMediaButtons, ContactForm } from "@/components";
-//Images
-import { ContactAnimation } from "@/images";
-//Next
-import Image from "next/image";
+import { SocialMediaButtons, ContactForm, LottieAnimation } from "@/components";
+//Animations
+import { ContactAnimation } from "@/animations";
 
 const Contact = () => {
   return (
     <section className="flex flex-col w-[95%] md:w-full place-self-center gap-5 mt-5 mb-5">
       <h1 className="font-bold text-xl md:text-4xl">Entre em contato</h1>
 
-      <div className="flex flex-col md:flex-row md:gap-60">
-        <div className="flex flex-col items-center justify-center gap-3 md:order-2">
-          <Image
+      <div className="flex flex-col md:flex-row">
+        <div className="flex flex-col items-center md:order-2 md:max-h-96">
+          <LottieAnimation
             src={ContactAnimation}
-            alt="entre em contato"
-            width={500}
-            height={500}
-            className="w-[95%] max-w-[320px] h-[205px]"
+            className="-mb-5 -mt-5 max-h-96"
           />
           <SocialMediaButtons />
         </div>
 
-        <div className="flex flex-wrap w-full md:w-fit justify-center md:order-1">
+        <div className="flex flex-wrap w-full md:w-fit md:order-1">
           <ContactForm />
         </div>
       </div>
