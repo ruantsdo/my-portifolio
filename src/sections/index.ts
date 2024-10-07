7310f78dd8ef3aeab2f84cbaf127f87da0cbbc7c
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-07 13:16:54 -0300

    :sparkles: contact section has created

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,5 +1,6 @@
 import Presentation from "./presentation";
 import Skills from "./skills";
 import Projects from "./projects";
+import Contact from "./contact";
 
-export { Presentation, Skills, Projects };
+export { Presentation, Skills, Projects, Contact };
