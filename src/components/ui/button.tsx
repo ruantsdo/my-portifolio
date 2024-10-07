6159b6ad5c1084e1362a7c30c184e6c88f03db60
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2024-10-07 12:55:29 -0300

    :lipstick: conditioning button animation

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 hover:scale-[110%] transition-all",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:pointer-events-none disabled:opacity-50 md:hover:scale-[110%] transition-all",
   {
     variants: {
       variant: {
