765da423cfc501235ddd6c552e30362027583de8
Ruan Tiago <35500071+ruantsdo@users.noreply.github.com>
2025-07-16 11:09:29 -0300

    ✨ create card information container

---
+++ src/components/ProjectCard/cardInformation.tsx
@@ -0,0 +1,25 @@
+//Shadcn UI
+import { CardDescription, CardTitle } from "@/components/ui/card";
+//Types
+import { Project } from "@/types";
+
+interface Props {
+  title: Project["title"];
+  description: Project["description"];
+}
+
+const CardInformation = ({ title, description }: Props) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <CardTitle className="text-center font-bold text-sm mt-1 md:mt-0">
+        {title}
+      </CardTitle>
+
+      <CardDescription className="text-ellipsis overflow-hidden text-wrap line-clamp-5 font-medium text-white">
+        {description}
+      </CardDescription>
+    </div>
+  );
+};
+
+export default CardInformation;
